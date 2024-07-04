88e9b0d5db9ff33f3469bc4784b16063ee59797a
Vidit Saxena <viditsaxena0000@gmail.com>
2024-07-05 05:11:58 +0530

    sighsssss

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -56,9 +56,7 @@ const Header = () => {
     <header className='h-16 shadow-md bg-white fixed w-full z-40'>
       <div className=' h-full container mx-auto flex items-center px-4 justify-between'>
             <div className=''>
-                <Link to={"/"}>
-                    <Logo w={90} h={50}/>
-                </Link>
+                
             </div>
 
             <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
